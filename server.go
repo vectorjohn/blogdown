d6b9f373aeac6e7312db56b65aa55fcffb735f21
John Reeves <vectorjohn@gmail.com>
2015-03-23 00:46:43 -0700

    supports wrapper html template.

--- server.go
+++ server.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"fmt"
+	"code.google.com/p/gorilla/mux"
 	"github.com/russross/blackfriday"
 	"io/ioutil"
 	"net/http"
 	"os"
 	"strconv"
+	"text/template"
+	"bytes"
 )
 
 type ServerConfig struct {
@@ -22,11 +25,31 @@ type TestData struct {
 	baz string
 }
 
+type BufferedLoader struct {
+	err error
+}
+
+func (this *BufferedLoader) load(fileName string) []byte {
+	if this.err != nil {
+		return []byte{}
+	}
+
+	data, err := loadFile(fileName)
+	this.err = err
+	return data
+}
+
+func newLoader() *BufferedLoader {
+	return &BufferedLoader{}
+}
+
 var config *ServerConfig = &ServerConfig{"pages", "www", "index", 4003}
 
 func main() {
+
+	r := mux.NewRouter()
 	/*
-		data, err := loadFile(config.Index)
+		data, err := loadFile(config.Index + ".md")
 		if err != nil {
 			fmt.Println( err.Error() )
 			return
@@ -36,7 +59,7 @@ func main() {
 		fmt.Println(string(output))
 	*/
 
-	if 1==1 {
+	if false {
 		docstore := &FSDocumentStore{Root: "data"}
 		/*
 		var out Document
@@ -55,10 +78,10 @@ func main() {
 		return
 	}
 
-	http.HandleFunc("/admin", configHandler(config, admin_handler))
+	//r.HandleFunc("/admin", configHandler(&config, admin_handler))
 
 	indexServer := pageServer("index")
-	http.HandleFunc("/", func(resp http.ResponseWriter, req *http.Request) {
+	r.HandleFunc("/", func(resp http.ResponseWriter, req *http.Request) {
 		if req.URL.Path == "/" {
 			indexServer(resp, req)
 			return
@@ -69,16 +92,27 @@ func main() {
 
 	listenOn := "localhost:" + strconv.Itoa(config.Port)
 	fmt.Println("Listening on: " + listenOn)
+
+	http.Handle("/", r)
 	http.ListenAndServe(listenOn, nil)
 }
 
-func configHandler(conf ServerConfig, handler func(resp http.ResponseWriter, req *http.Request)) {
-	handler(conf, resp, req)
+func configHandler(conf *ServerConfig, handler func(*ServerConfig, http.ResponseWriter, *http.Request)) http.HandlerFunc {
+	return func(resp http.ResponseWriter, req *http.Request) {
+		handler(conf, resp, req)
+	}
 }
 
 func pageServer(page string) func(http.ResponseWriter, *http.Request) {
-	data, err := loadFile(page)
-	if err != nil {
+	return pageTemplateServer(page, "default.tpl.html")
+}
+
+func pageTemplateServer(page, tmplName string) func(http.ResponseWriter, *http.Request) {
+	loader := newLoader()
+	data := loader.load(page + ".md")
+	templateText := loader.load(tmplName)
+
+	if loader.err != nil {
 		if page == "404" {
 			return internalServerError
 		}
@@ -87,6 +121,20 @@ func pageServer(page string) func(http.ResponseWriter, *http.Request) {
 	}
 
 	data = blackfriday.MarkdownCommon(data)
+
+	tmpl := template.Must(template.New(tmplName).Parse(string(templateText)))
+
+	type TemplateData struct {
+		Title string
+		Body string
+	}
+
+	templateData := TemplateData{Body: string(data)}
+
+	buf := &bytes.Buffer{}
+	tmpl.Execute(buf, templateData)
+	data, _ = ioutil.ReadAll(buf)
+
 	return func(resp http.ResponseWriter, req *http.Request) {
 		resp.Write(data)
 	}
@@ -97,7 +145,7 @@ func internalServerError(resp http.ResponseWriter, req *http.Request) {
 }
 
 func loadFile(name string) ([]byte, error) {
-	file, err := os.Open(config.PagePath + "/" + name + ".md")
+	file, err := os.Open(config.PagePath + "/" + name)
 	if err != nil {
 		return nil, err
 	}
